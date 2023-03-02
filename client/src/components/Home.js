681c1da1486ed46ede708b7a392328cce10550d2
MattHerringshaw1 <matt.herringshaw@yahoo.com>
2023-03-02 14:42:03 -0600

    main components

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,1 +1,19 @@
-// test
+
+
+function Home() {
+
+    return(
+        <>
+            <h1>testing</h1>
+
+            <div>
+                <h1>Lubbock</h1>
+            </div>
+            <div>
+                <h1>Dallas</h1>
+            </div>
+        </>
+    )
+}
+
+export default Home
